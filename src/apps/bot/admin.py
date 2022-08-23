ae5144a28b73cde686a3c3f41b426ea5d49a68d8
Kirill.Shershen <kirill.shershen@alao.ch>
2022-08-23 09:57:25 +0300

    added admin models

--- src/apps/bot/admin.py
+++ src/apps/bot/admin.py
@@ -1,2 +1,6 @@
-# from django.contrib import admin
-# Register your models here.
+from django.contrib import admin
+
+from apps.bot.models import SearchQuery, QueryResult
+
+admin.site.register(SearchQuery, admin.ModelAdmin)
+admin.site.register(QueryResult, admin.ModelAdmin)
--- src/config/settings.py
+++ src/config/settings.py
@@ -91,11 +91,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": "db",
-        "PORT": 5432,
+        "NAME": os.environ.get("DB_NAME", "postgres"),
+        "USER": os.environ.get("DB_USER", "postgres"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
+        "HOST": os.environ.get("DB_HOST", "db"),
+        "PORT": os.environ.get("DB_PORT", 5432),
     }
 }
 

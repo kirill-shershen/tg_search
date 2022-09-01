ce5595fd5f13bbdb3867bdc48482f49b448f3535
Kirill.Shershen <kirill.shershen@alao.ch>
2022-09-01 22:49:30 +0300

    added whitenoise

--- src/config/base.py
+++ src/config/base.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
